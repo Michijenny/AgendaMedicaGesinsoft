614bdd35083caa97ead62f30a05d96c24361f74d
FicctionZ <santiagogcan@gmail.com>
2024-02-01 15:07:12 -0500

    Test

---
+++ bin/src/main/resources/sqlData.sql
@@ -0,0 +1,3 @@
+<!--INSERT INTO doctor (idDoctor, nombre ,clavesecreta, comentarios, direccion, especialidad, telefono, clave, notaAuto, nota, comparte, cfg, cfgsec, email)-->
+<!--VALUES ('Juan', '1234' , 'comentarios', 'tarqui', 'medico cirugano', '2878689','1234', 'notaA', 'nota', 'comparte', 'cfg', 'cfgsec', 'juan@gmail.com');-->
+
